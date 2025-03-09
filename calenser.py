b0bbdb23dab27ebfa78a3974b56505d8f2b5016a
Sohanur Rahaman Sohan <152248019+qwopiye@users.noreply.github.com>
2025-03-09 23:12:25 +0600

    calenser.py
    data and month and year

---
+++ calenser.py
@@ -0,0 +1,34 @@
+#import calender
+import calendar
+#input year
+yy=int(input("Enter the year :"))
+#input month
+mm=int(input("Enter the month :"))
+#conditional Statement
+if yy>1:
+    print(calendar.month(yy,mm))
+elif yy>2:
+    print(calendar.month(yy,mm))
+elif yy>3:
+    print(calendar.month(yy,mm))
+    
+elif yy>4:
+    print(calendar.month(yy,mm))
+elif yy>5:
+    print(calendar.month(yy,mm))
+        
+elif yy>6:
+    print(calendar.month(yy,mm))
+       
+elif yy>7:
+    print(calendar.month(yy,mm))
+elif yy>8:
+    print(calendar.month(yy,mm))  
+elif yy>9:
+    print(calendar.month(yy,mm))
+elif yy>10:
+    print(calendar.month(yy,mm))
+elif yy>11:
+    print(calendar.month(yy,mm))
+else:
+    print(calendar.month(yy,mm))      
